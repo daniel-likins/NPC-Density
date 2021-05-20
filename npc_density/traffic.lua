112fd11fe7202eb44cb200787ed2375f82437746
thedannyl <84494309+thedannyl@users.noreply.github.com>
2021-05-20 08:28:43 -0500

    Update traffic.lua

--- npc_density/traffic.lua
+++ npc_density/traffic.lua
@@ -3,7 +3,7 @@ local config = {
     trafficFrequency = 1.0, -- Controls the amount of vehicles driving around and also controls the amount of parked vehicles (0.0 = None | 0.5 = Half as Many| 1.0 = Normal | 2.0 = Twice as Many)
 }
 
--- DO NOT Touch Anything Below Here Unless You Know What Your Doing
+-- I Would Not Touch Anything Below Here Unless You Know What Your Doing
 
 Citizen.CreateThread(function()
     while true do
