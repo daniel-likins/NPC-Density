5af651f0817ff3e4f70e79da906a99d84d23f59e
thedannyl <84494309+thedannyl@users.noreply.github.com>
2022-04-30 13:01:32 -0500

    Delete credits.lua

--- npc_density/credits.lua
+++
@@ -1,9 +0,0 @@
-Script Written By Danny L -- Inspired By Jeva - https://www.youtube.com/channel/UCI7x329xu2rLbtVvFPVIhiQ
-
-If you have any questions feel free to send me a dm on discord -- dannyl#7448
-
-I am very new to this so there may be a few minor bugs.
-
-|-----------------------------------------------------------------|
-| Join Every Day Roleplay Today! -- https://discord.gg/vgcNJe4Zn8 |
-|-----------------------------------------------------------------|
